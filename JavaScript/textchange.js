9625e621165ad3a731d22f89aca934d99c7e9057
Vishal7017 <wavhalev@gmail.com>
2021-05-29 19:05:21 +0530

    JS

---
+++ JavaScript/textchange.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	$('#fade_in_tasks').click(function() {
+		$('ul').fadeOut();
+  });
+});
+
+
+// $(document).ready(function () {
+// 	$('#fade_in_tasks').click(function() {
+// 		$('ul').fadeIn();
+//   });
+// });
